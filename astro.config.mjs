b3a8fb1bfec8c1dffde5083e75b13806212e7561
A. Ounissi <103394069+A-Ounissi@users.noreply.github.com>
2025-12-29 21:28:57 -0600

    Change base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ahmed-ounissi.github.io',
-  base: '/devportfolio',
+  base: '/Portfolio',
   vite: {
     plugins: [tailwindcss()],
   },
